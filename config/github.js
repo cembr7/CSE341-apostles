876e226afcf0312bac2107f4c2e41ec5737c1cfa
cembr7 <cembr7@byui.edu>
2025-10-31 15:27:34 -0500

    add config for github oauth

---
+++ config/github.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+module.exports = {
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: `${process.env.BASE_URL}/auth/github/callback`
+};
